d9b559dda85d53ac014ed8ddd9915fc82b051868
MarceHM <47231554+MarceHM@users.noreply.github.com>
2020-05-08 21:03:59 -0500

    Add files via upload

--- src/control/main.java
+++ src/control/main.java
@@ -1,10 +1,14 @@
 package control;
 
-public class main {
+import vista.VentanaInicio;
 
+public class main {	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VentanaInicio ventInicio = new VentanaInicio();
+		ventInicio.setVisible(true);
+		
 	}
 
 }
